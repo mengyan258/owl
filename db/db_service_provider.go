7cfde1b057e52250d536f3084ba8b0400a89fc6b
guoliang <bit-labs@qq.com>
2025-07-23 17:36:07 +0800

    feat: sqlite 生成的数据库文件，默认放到配置目录下面

--- db/db_service_provider.go
+++ db/db_service_provider.go
@@ -3,7 +3,9 @@ package db
 import (
 	"bit-labs.cn/owl/conf"
 	"bit-labs.cn/owl/contract/foundation"
+	"bit-labs.cn/owl/contract/log"
 	"gorm.io/gorm"
+	"path/filepath"
 )
 
 var _ foundation.ServiceProvider = (*DBServiceProvider)(nil)
@@ -14,11 +16,15 @@ type DBServiceProvider struct {
 
 func (i *DBServiceProvider) Register() {
 
-	i.app.Register(func(c *conf.Configure) *gorm.DB {
+	i.app.Register(func(c *conf.Configure, l log.Logger) *gorm.DB {
 		var opt Options
 		err := c.GetConfig("database.db", &opt)
 		if err != nil {
 			panic(err)
+		}
+		if opt.Driver == Sqlite {
+			opt.Host = filepath.Join(i.app.ConfigPath(""), opt.Host)
+			l.Debug("sqlite path:", opt.Host)
 		}
 		return InitDB(&opt)
 	})
