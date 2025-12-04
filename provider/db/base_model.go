0b824e25ca60a427a97c36e629a9c0f9c67e7633
guoliang <bit-labs@qq.com>
2025-12-04 11:49:26 +0800

    feat: 提供 BaseModel 给子系统继承

--- provider/db/base_model.go
+++ provider/db/base_model.go
@@ -2,12 +2,16 @@ package db
 
 import (
 	"gorm.io/gorm"
+	"time"
 )
 
 type BaseModel struct {
-	gorm.Model
-	CreatorID string `gorm:"comment:创建人" json:"creatorID"` // 创建人
-	UpdaterID string `gorm:"comment:修改人" json:"updaterID"` // 修改人
+	ID        uint `gorm:"primarykey"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt gorm.DeletedAt `gorm:"index"`
+	CreatorID string         `gorm:"comment:创建人" json:"creatorID"` // 创建人
+	UpdaterID string         `gorm:"comment:修改人" json:"updaterID"` // 修改人
 }
 
 func (i *BaseModel) BeforeCreate(tx *gorm.DB) (err error) {
