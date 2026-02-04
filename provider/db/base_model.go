a2d3f06d263e31138581c4c866774a3e77986690
guoliang <bit-labs@qq.com>
2026-02-04 16:51:00 +0800

    feat: 创建时，自动设置创建人，修改时，自动设置修改人

--- provider/db/base_model.go
+++ provider/db/base_model.go
@@ -9,12 +9,14 @@ import (
 )
 
 type BaseModel struct {
-	ID        uint            `gorm:"primarykey" json:"id,string"`
-	CreatedAt *time.Time      `json:"createdAt,omitempty"`
-	UpdatedAt *time.Time      `json:"updatedAt,omitempty"`
-	DeletedAt *gorm.DeletedAt `gorm:"index" json:"deletedAt,omitempty"`       // 删除时间
-	CreatorID string          `gorm:"comment:创建人" json:"creatorID,omitempty"` // 创建人
-	UpdaterID string          `gorm:"comment:修改人" json:"updaterID,omitempty"` // 修改人
+	ID          uint            `gorm:"primarykey" json:"id,string"`
+	CreatedAt   *time.Time      `json:"createdAt,omitempty"`
+	UpdatedAt   *time.Time      `json:"updatedAt,omitempty"`
+	DeletedAt   *gorm.DeletedAt `gorm:"index" json:"deletedAt,omitempty"`           // 删除时间
+	CreatorID   string          `gorm:"comment:创建人" json:"creatorID,omitempty"`     // 创建人
+	CreatorName string          `gorm:"comment:创建人名称" json:"creatorName,omitempty"` // 创建人名称
+	UpdaterID   string          `gorm:"comment:修改人" json:"updaterID,omitempty"`     // 修改人
+	UpdaterName string          `gorm:"comment:修改人名称" json:"updaterName,omitempty"` // 修改人名称
 }
 
 func (i *BaseModel) BeforeCreate(tx *gorm.DB) (err error) {
@@ -27,5 +29,17 @@ func (i *BaseModel) BeforeCreate(tx *gorm.DB) (err error) {
 	if i.ID == 0 {
 		i.ID = cast.ToUint(id)
 	}
+
+	ctx := tx.Statement.Context
+
+	i.CreatorID = cast.ToString(ctx.Value("user_id"))
+	i.CreatorName = cast.ToString(ctx.Value("username"))
+	return
+}
+func (i *BaseModel) BeforeUpdate(tx *gorm.DB) (err error) {
+	ctx := tx.Statement.Context
+
+	i.UpdaterID = cast.ToString(ctx.Value("user_id"))
+	i.UpdaterName = cast.ToString(ctx.Value("username"))
 	return
 }
